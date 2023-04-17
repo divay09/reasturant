aa2962c5a86f91cfc9e862d3401fe392d7aa093b
divay09 <110602433+divay09@users.noreply.github.com>
2023-04-18 00:10:50 +0530

    Add files via upload

---
+++ src/data/data.js
@@ -0,0 +1,52 @@
+import Dosa from "../images/dosa.jpg";
+import Chola from "../images/chhola.jpg";
+import MasalaDosa from "../images/masala.jpg";
+import Paneer from "../images/paneer.jpg";
+import Gujrati from "../images/gujrati.jpeg";
+import Haryanvi from "../images/Haryanvi.jpg";
+
+export const MenuList = [
+  {
+    name: "Chola Bhature",
+    description:
+      "Top selling Food of our Resturant",
+    image: Chola,
+    price: 250,
+  },
+  {
+    name: "Plain Dosa",
+    description:
+      "South Indian plain dosa",
+    image: Dosa,
+    price: 200,
+  },
+  {
+    name: "Sarso ka saag and makke di Roti",
+    description:
+      "Thet Haryanvi Food",
+    image: Haryanvi,
+    price: 100,
+  },
+  {
+    name: "Masala Dosa",
+    description:
+      "Authentic South Indian Food",
+    image: MasalaDosa,
+    price: 100,
+  },
+  {
+    name: "Paneer",
+    description:
+      "Shudh desi paneer",
+    image: Paneer,
+    price: 400,
+  },
+  {
+    name: "Gujrati Thali",
+    description:
+      "Best Gujrati dishes with best sweets",
+    image: Gujrati,
+    price: 500,
+  },
+  
+];
